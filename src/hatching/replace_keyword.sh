75c331ddd6638616ceadc3bc464e5f77760e9766
tomochin <gongga5515@163.com>
2013-03-03 13:56:05 +0800

    change to depth tex

---
+++ src/hatching/replace_keyword.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+BASEDIR=`pwd`
+TASKDIRS="pretask task1 task2 task3 task4 task5 task6"
+KEY1="phong"
+KEY2="hatching"
+
+
+for ts in ${TASKDIRS}
+do
+    echo -e "replace keyword \"${KEY1}\" to \"${KEY2}\" for ${ts}..."
+    sed -i 's/'${KEY1}'/'${KEY2}'/g' ${BASEDIR}/${ts}/*.{cpp,h}
+done
+
